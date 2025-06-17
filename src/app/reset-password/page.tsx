79b1487fc62351925c06f2fb9e4ee95e9c44bd13
Priest ☦ <onaopemipomichael1999@gmail.com>
2025-06-17 10:29:03 +0100

    jhf

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -8,3 +8,11 @@
 // };
 
 // export default ResetPasswordPage;
+
+export default function ResetPasswordPage() {
+  return (
+    <div className="text-red-500 text-center mt-10">
+      Reset password page under maintenance 🚧
+    </div>
+  );
+}
